9e5df03cb19a9b173856915a5beaefa0ca654410
yayoqs <67708300+yayoqs@users.noreply.github.com>
2026-06-13 14:08:27 -0400

    update ForceExports.cpp

--- ForceExports.cpp
+++ ForceExports.cpp
@@ -49,6 +49,6 @@ void ForceExports_Dummy() {
         (void)f;
     }
 
-    // VehicleConstraint: forzar inclusión de la clase usando un método existente (GetWheelCount)
-    (void)&JPH::VehicleConstraint::GetWheelCount;
+    // VehicleConstraint: forzar inclusión de la clase usando un método existente (Reset)
+    (void)&JPH::VehicleConstraint::Reset;
 }
